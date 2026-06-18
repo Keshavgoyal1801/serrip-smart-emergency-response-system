e7b3ba60df3e933eb00787fb844d76bfc3a6d986
Keshav Goyal <keshavgoyal0110@gmail.com>
2026-06-18 10:26:21 +0530

    feat(auth): implement CustomUserDetailsService

---
+++ backend/src/main/java/com/serrip/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.serrip.backend.security;
+
+import com.serrip.backend.entity.User;
+import com.serrip.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(
+            String username)
+            throws UsernameNotFoundException {
+
+        User user =
+                userRepository
+                        .findByUsername(username)
+                        .orElseThrow(() ->
+                                new UsernameNotFoundException(
+                                        "User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
