ef84fa90a70177bbe6a01f5cb967e202fed725a8
Keshav Goyal <keshavgoyal0110@gmail.com>
2026-06-18 10:49:35 +0530

    feat(auth): configure Spring Security and add authentication controller

---
+++ backend/src/main/java/com/serrip/backend/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.serrip.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/api/auth/test")
+    public String test() {
+
+        return "Authentication API is Public";
+    }
+}
